0de410453fda5ad5ed3603be0a60fec51e016b9f
Islanderrobotics <75331800+Islanderrobotics@users.noreply.github.com>
2021-07-11 20:48:50 -0400

    this is a pretty fun sorting algorithm

--- main.py
+++ main.py
@@ -1,41 +1,32 @@
-def insertionSort(b):
-    for i in range(1, len(b)):
-        up = b[i]
+def insertio(array):
+    for i in range(1,len(array)):
+        up = array[i]
         j = i - 1
-        while j >= 0 and b[j] > up:
-            b[j + 1] = b[j]
-            j -= 1
-        b[j + 1] = up
-    return b
+        while j >= 0 and array[j]>up:
+            array[j+1]= array[j]
+            j-=1
+        array[j+1] = up
+    return (array)
 
-
-def bucketSort(x):
-    arr = []
-    slot_num = 10  # 10 means 10 slots, each
-    # slot's size is 0.1
-    for i in range(slot_num):
-        arr.append([])
-
-    # Put array elements in different buckets
-    for j in x:
-        index_b = int(slot_num * j)
-        arr[index_b].append(j)
-
-    # Sort individual buckets
-    for i in range(slot_num):
-        arr[i] = insertionSort(arr[i])
-
-    # concatenate the result
+def Bucketsort(arr):
+    array = []
+    slot_numbers = 10
+    for i in range(slot_numbers):
+        array.append([])
+    for j in arr:
+        index_b = int(slot_numbers*j)
+        array[index_b].append(j)
+    for i in range(slot_numbers):
+        array[i] = insertio(array[i])
     k = 0
-    for i in range(slot_num):
-        for j in range(len(arr[i])):
-            x[k] = arr[i][j]
-            k += 1
-    return x
+    for i in range(slot_numbers):
+        for j in range(len(array[i])):
+            arr[k] = array[i][j]
+            k+=1
+    return arr
 
 
-# Driver Code
-x = [0.897, 0.565, 0.656,
-     0.1234, 0.665, 0.3434]
-print("Sorted Array is")
-print(bucketSort(x))
+if __name__ == "__main__":
+    array = [0.897,0.565,0.656,0.5432,0.5143]
+    print("this is the sorted array")
+    print(Bucketsort(array))
